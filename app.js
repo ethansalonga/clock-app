79cf9a1ea0a8fa0e5f5cd5972dd44c1b754892d4
Ethan Salonga <ethansalonga28@gmail.com>
2022-12-28 09:58:40 -0500

    set interval for handles function

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const secondHandle = document.querySelector(".handle__second")
 const minuteHandle = document.querySelector(".handle__minute")
 const hourHandle = document.querySelector(".handle__hour")
 
-const d = new Date()
-const hours = d.getHours()
-const minutes = d.getMinutes()
-const seconds = d.getSeconds()
+setInterval(() => {
+  setHandles()
+}, 1000)
 
-console.log(seconds * 6)
+function setHandles() {
+  console.log("called")
+  const d = new Date()
 
-hourHandle.style.transform = `translateX(-50%) rotate(${hours * 30}deg)`
-minuteHandle.style.transform = `translateX(-50%) rotate(${minutes * 6}deg)`
-secondHandle.style.transform = `translateX(-50%) rotate(${seconds * 6}deg)`
+  const hours = d.getHours()
+  const minutes = d.getMinutes()
+  const seconds = d.getSeconds()
+
+  hourHandle.style.transform = `translateX(-50%) rotate(${hours * 30}deg)`
+  minuteHandle.style.transform = `translateX(-50%) rotate(${minutes * 6}deg)`
+  secondHandle.style.transform = `translateX(-50%) rotate(${seconds * 6}deg)`
+}
+
