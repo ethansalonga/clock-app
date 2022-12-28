3eb7799c9d842e6305d1bbc97747930891a188c6
Ethan Salonga <ethansalonga28@gmail.com>
2022-12-28 10:41:23 -0500

    make minutes/hours dynamic

--- app.js
+++ app.js
@@ -13,11 +13,17 @@ function setHands() {
   const seconds = d.getSeconds()
   const millis = d.getMilliseconds()
 
+  const extraHoursAngle = minutes * 0.5
+  const hoursAngle = hours * 30 + extraHoursAngle
+
+  const extraMinutesAngle = seconds * 0.1
+  const minutesAngle = minutes * 6 + extraMinutesAngle
+
   const extraSecondsAngle = millis * 0.006
   const secondsAngle = seconds * 6 + extraSecondsAngle
 
-  hourHand.style.transform = `translateX(-50%) rotate(${hours * 30}deg)`
-  minuteHand.style.transform = `translateX(-50%) rotate(${minutes * 6}deg)`
+  hourHand.style.transform = `translateX(-50%) rotate(${hoursAngle}deg)`
+  minuteHand.style.transform = `translateX(-50%) rotate(${minutesAngle}deg)`
   secondHand.style.transform = `translateX(-50%) rotate(${secondsAngle}deg)`
   millisHand.style.transform = `translateX(-50%) rotate(${360 * millis / 1000}deg)`
   requestAnimationFrame(setHands)
